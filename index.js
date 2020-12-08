733c6fc5cc53cd41d8753556cd4e1a6cdceee03a
zamFe <zamfel18@student.kristiania.no>
2020-12-08 14:03:02 +0100

    added links and emojis, plus time

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on('message', async msg => {
             'available': ['div .product-list-item', function ($d) {
                 return $d.text();
             }],
-            'link': ['a .image-container', function ($a) {
+            'link': ['div .product-list-item', function ($a) {
                 return $a.attr('href');
             }]
         }, function (err, result) {
@@ -100,15 +100,22 @@ function checkForUpdate(result) {
     return false;
 }
 
+function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
 setInterval(function () {
-    var komplett = htmlToJson.request('https://www.komplett.no/category/10412/datautstyr/pc-komponenter/skjermkort?nlevel=10000%C2%A728003%C2%A710412&cnet=Grafikkprosessor_A00247%20%20%C2%A7NVIDIA%20GeForce%20RTX%203080&cnet=Grafikkprosessor_A00247%20%20%C2%A7NVIDIA%20GeForce%20RTX%203090&cnet=Grafikkprosessor_A00247%20%20%C2%A7NVIDIA%20GeForce%20RTX%203070&stockStatus=InStock', {
+    var komplett = htmlToJson.request('https://www.komplett.no/search?q=rtx%2030&stockStatus=InStock&sort=titleSort%3AASCENDING&nlevel=10000%C2%A728003%C2%A710412', {
         'card': ['h2', function ($h) {
             return $h.text();
         }],
         'available': ['div .product-list-item', function ($d) {
             return $d.text();
         }],
-        'price': ['a .image-container', function ($a) {
+        'link': ['div .product-link', function ($a) {
             return $a.attr('href');
         }]
     }, function (err, result) {
@@ -121,21 +128,25 @@ setInterval(function () {
                 console.log(err);
             }
         });
+        var links = result.link;
+        let uniqueLinks = [...new Set(links)];
+        console.log(uniqueLinks);
         var cardStr = "\n *Currently listed cards:* \n\n";
         for (let i = 0; i < result.card.length; i++) {
             cardStr += JSON.stringify(result.card[i]) + "\n";
             cardStr += "status: ";
             if (result.available.length > i) {
                 if (JSON.stringify(result.available[i]).includes("Motta Varsel")) {
-                    cardStr += "not available \n";
+                    cardStr += "not available :clock1:  \n";
                     cardStr += "\n";
                 } else if (JSON.stringify(result.available[i]).includes("Ikke på lager")) {
-                    cardStr += "not in stock\n";
-                    //cardStr += "buy here: " + result.link[i] + "\n";
+                    cardStr += "not in stock :warning: \n";
+                    cardStr += "https://www.komplett.no/" + uniqueLinks[i] + "\n";
                     cardStr += "\n";
                 } else if (JSON.stringify(result.available[i]).includes("stk. på lager")) {
-                    cardStr += "in stock!\n";
-                    //cardStr += "buy here: " + result.link[i] + "\n";
+                    var stkIndex = JSON.stringify(result.available[i]).indexOf("stk. på lager");
+                    cardStr += "in stock! :white_check_mark: Cards Left: " + JSON.stringify(result.available[i]).substring(stkIndex - 3, stkIndex - 1) + "\n";
+                    cardStr += "https://www.komplett.no/" + uniqueLinks[i] + "\n";
                     cardStr += "\n";
                 }
             } else {
@@ -143,11 +154,26 @@ setInterval(function () {
                 cardStr += "\n";
             }
         }
+
+        cardStr += "**What do these symbols mean?**\n";
+        cardStr += ":clock1: The card is listed, but is not available for purchase  (yet) \n";
+        cardStr += ":warning: The card is listed for purchase, but there is no stock left (too late or too early) \n";
+        cardStr += ":white_check_mark: The card is currently in stock! \n";
+
+        var currentdate = new Date(); 
+        var datetime = "\n Last updated: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " Kl. "  
+                + addZero(currentdate.getHours()) + ":" +
+                + addZero(currentdate.getMinutes());
+
+        cardStr += datetime;
+
         //message = new Discord.Message(client, cardStr, client.channels.cache.get("596121073867817000"));
         if(previousMessage != undefined && previousMessage != null) {
             previousMessage.delete();
         }
-        client.channels.cache.get("596121073867817000").send(cardStr).then(msg => previousMessage = msg);
+        client.channels.cache.get("785810248782708796").send(cardStr).then(msg => previousMessage = msg);
         prevStatusList = result.available;
     });
 }, 1000); // 60 * 1000 milsec
